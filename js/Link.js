5a7e96e7497974ca211f2b2713a1baceab414f33
ishana29 <85287354+ishana29@users.noreply.github.com>
2021-08-10 15:26:50 +0530

    Add files via upload

---
+++ js/Link.js
@@ -0,0 +1,15 @@
+class Link{
+    constructor(bodyA,bodyB){
+        var lastlink=bodyA.body.bodies.lenth-2;
+        this.link=Constraint.create({
+            bodtA:bodyA.body.bodies[lastlink],
+            pointA:{x:0,y:0},
+            bodyB:bodyB.body,
+            pointB:{x:0,y:0},
+            lenthe:10,
+            stiffness:0.8
+        });
+        
+        World.add(world,this.link);
+    }
+}
